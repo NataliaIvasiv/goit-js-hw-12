c5e33dccf0ebc3966322d16b3733aa3d2147a60b
NataliaIvasiv <143248403+NataliaIvasiv@users.noreply.github.com>
2024-02-18 00:44:25 +0200

    done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export async function fetchImages(keyWord) {
+export async function fetchImages(keyWord, currentPage) {
     const API_KEY = '42236651-09dd8ef8cae726de85d6e38a7'
     const BASE_URL = 'https://pixabay.com';
     const END_POINT = '/api?';
@@ -10,7 +10,7 @@ export async function fetchImages(keyWord) {
         orientation: "horizontal",
         safesearch: true,
         per_page: 15,
-        // page: currentPage,
+        page: currentPage,
     }
         
     const url = `${BASE_URL}${END_POINT}`;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-
-
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
@@ -13,7 +11,7 @@ import { imagesTemplate } from "./js/render-functions";
 const form = document.querySelector('form');
 const imagesList = document.querySelector(".gallery")
 const loader = document.querySelector(".loader")
-
+const loadButton = document.querySelector(".load-btn")
 
 const options = {
   captions: true,
@@ -23,19 +21,25 @@ const options = {
   captionPosition: 'bottom',
   captionDelay: 250,
 };
-
+let gallery = new SimpleLightbox('.gallery a', options);
+gallery.on('show.simplelightbox');
 
 let keyWord;
-form.addEventListener('submit', onFormSubmit)
+let page;
+let maxPage;
 
+
+form.addEventListener('submit', onFormSubmit);
+loadButton.addEventListener('click', onLoadMoreButton);
 
 
 async function onFormSubmit(event) {
         event.preventDefault();
     imagesList.innerHTML = '';
+    page = 1;
 
     keyWord = event.target.elements.search.value.trim();
-    loader.classList.remove('is-hidden');
+    showLoader();
 
     if (!keyWord) {
     iziToast.warning({
@@ -43,23 +47,26 @@ async function onFormSubmit(event) {
         position: "topLeft",
     message: 'This field is required',
     });
-        loader.classList.add('is-hidden')
+        hideLoader();
     return;
   }
     try {
-        const images = await fetchImages(keyWord);
+        const images = await fetchImages(keyWord, page);
+        maxPage = Math.ceil(images.total / 15);
+        console.log(maxPage);
         showEmptySearchResult(images);
             renderImages(images);
-             let gallery = new SimpleLightbox('.gallery a', options);
-    gallery.on('show.simplelightbox');
+             
+    
     gallery.refresh();
-            loader.classList.add('is-hidden');
+        hideLoader();
+       
     } catch (error) {
         console.log(error)
         };
-    
+     
     event.target.reset();
-    
+    showLoadMore();
 }
 
 
@@ -80,11 +87,54 @@ function renderImages(images) {
 }
 
 
+async function onLoadMoreButton(event) {
+    hideLoadMore();
+    showLoader();
+    page += 1;
+        const images = await fetchImages(keyWord, page);
+            renderImages(images);
+             
+    gallery.refresh();
+    hideLoader();
+        showLoadMore();
+    checkGalleryEnd();
+
+     const height =
+    imagesList.firstElementChild.getBoundingClientRect().height;
+
+  scrollBy({
+    behavior: 'smooth',
+    top: `${2 * height}`,
+  });
+}
 
 
 
 
+function checkGalleryEnd() {
+    if (page >= maxPage) {
+        hideLoadMore();
+       iziToast.info({
+    message: "We're sorry, but you've reached the end of search results.",
+          
+    position: 'topRight'
+});
 
+    }
+}
+function showLoader() {
+    loader.classList.remove('is-hidden');
+}
+function hideLoader() {
+    loader.classList.add('is-hidden');
+}
+function showLoadMore() {
+    loadButton.classList.remove('is-hidden');
+}
+
+function hideLoadMore() {
+    loadButton.classList.add('is-hidden')
+}
 
 
 
